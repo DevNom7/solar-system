784e43bf119bbdade01448fb8e4e58220053063d
Naim Lindsay <N1L4792@gmail.com>
2023-09-25 21:19:18 -0700

    update to git

--- js/solar.js
+++ js/solar.js
@@ -1,3 +1,4 @@
 function showMessage() {
   alert('Welcome to Nom\'s Solar System! This project was created to showcase my advanced understanding of CSS techniques while incorporating basic JavaScript. Enjoy exploring \!');
 }
+// Copyright: Naim Lindsay aka Nom September 25,2023 
