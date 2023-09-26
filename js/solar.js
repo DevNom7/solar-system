97b6397ab4667610d9d91531df0d0e63a00874b1
Naim Lindsay <N1L4792@gmail.com>
2023-09-25 20:31:40 -0700

    First commit

---
+++ js/solar.js
@@ -0,0 +1,3 @@
+function showMessage() {
+  alert('Welcome to Nom\'s Solar System! This project was created to showcase my advanced understanding of CSS techniques while incorporating basic JavaScript. Enjoy exploring \!');
+}
